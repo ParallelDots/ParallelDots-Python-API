9aeab56a3e1a713f620acf6ade259c2081d6071a
Ahwan Kumar <ahwan.monu@gmail.com>
2015-07-11 19:53:30 +0530

    Added files for pypi

--- setup.py
+++ setup.py
@@ -2,16 +2,10 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-here = path.abspath(path.dirname(__file__))
-
-with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
-    long_description = f.read()
-
 setup(
     name='ParallelDots',
     version='0.2.0',
     description='Python Wrapper for ParallelDots API',
-    long_description=long_description,
     url='https://github.com/ParallelDots/ParallelDots-Python-API.git',
     author='Ahwan Kumar',
     author_email='ahwan@paralleldots.com',

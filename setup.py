3aaa6f5279b50de0137e430057a10f7cdaa5fb9b
Meghdeep Ray <meghdeepr@gmail.com>
2017-09-05 20:05:22 +0530

    Abuse

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read( fname ):
 
 setup(
     name="ParallelDots",
-    version="1.0.3",
+    version="1.0.4",
     description="Python Wrapper for ParallelDots APIs",
     long_description=read("DESCRIPTION.rst"),
     url="https://github.com/ParallelDots/ParallelDots-Python-API.git",

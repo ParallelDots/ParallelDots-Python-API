e783171edaa59bbfe1db3bcc2c3c8d7cf91312ac
Ahwan Kumar <ahwan.monu@gmail.com>
2018-01-08 18:50:54 +0530

    Update tests.py

--- tests.py
+++ tests.py
@@ -7,7 +7,7 @@ def test():
     taxonomy( "Narendra Modi is the prime minister of India" )
     ner( "Narendra Modi is the prime minister of India" )
     keywords( "Prime Minister Narendra Modi tweeted a link to the speech Human Resource Development Minister Smriti Irani made in the Lok Sabha during the debate on the ongoing JNU row and the suicide of Dalit scholar Rohith Vemula at the Hyderabad Central University." )
-    emotion("Did you hear the latest Porcupine Tree song ? It"s rocking !")
+    emotion("Did you hear the latest Porcupine Tree song ? It's rocking !")
     intent("Finance ministry calls banks to discuss new facility to drain cash")
     multilang("Me encanta jugar al baloncesto", "es")	# The text is encoded in the function
     abuse("you f**king a$$hole")

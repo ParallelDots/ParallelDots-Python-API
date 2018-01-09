650561c2b4074a4304473bbad6aad0e0dc3a18ac
Meghdeep Ray <meghdeepr@gmail.com>
2018-01-10 00:40:41 +0530

    Custom Classifier Wrapper Added

--- paralleldots/__init__.py
+++ paralleldots/__init__.py
@@ -1,4 +1,4 @@
-__all__= [ "taxonomy", "config", "ner", "similarity", "sentiment", "keywords", "emotion", "intent", "multilang", "sentiment_social", "get_usage" ]
+__all__= [ "taxonomy", "config", "ner", "similarity", "sentiment", "keywords", "emotion", "intent", "multilang", "sentiment_social", "get_usage", "custom_classifier" ]
 
 from paralleldots.taxonomy            import get_taxonomy            as taxonomy
 from paralleldots.ner                 import get_ner                 as ner
@@ -10,6 +10,7 @@ from paralleldots.emotion             import get_emotion             as emotion
 from paralleldots.intent              import get_intent              as intent
 from paralleldots.multilang           import get_multilang           as multilang
 from paralleldots.abuse               import get_abuse               as abuse
+from paralleldots.custom_classifier   import get_custom_classifier   as custom_classifier
 
 from paralleldots.usage               import get_usage               as usage
 
